f1d643e24e008b07f734868e7f6d789684d9cd13
Luis Eduardo Rodrigues <130309482+Luis-Eduardo-Rodrigues@users.noreply.github.com>
2023-07-26 13:06:25 -0300

    primeiro programa react

---
+++ 01-hello-world/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Meu primeiro programa em ReactJS</h1>
+      <p><b>Usei o vite porque é ligeiro de mais homi.</b></p>
+    </>
+  )
+}
+
+export default App
