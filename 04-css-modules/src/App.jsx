033c1f34b9f3f7a23b10c6e17a4872b28d73ca25
Luis Eduardo Rodrigues <130309482+Luis-Eduardo-Rodrigues@users.noreply.github.com>
2023-07-28 13:28:26 -0300

    css-modules

---
+++ 04-css-modules/src/App.jsx
@@ -0,0 +1,11 @@
+import Frase from "./components/Frase"
+
+function App() {
+  return (
+    <>
+      <Frase></Frase>
+    </>
+  )
+}
+
+export default App
---
+++ 04-css-modules/src/components/Frase.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from "./Frase.module.css"
+
+function Frase() {
+  return (
+    <>
+    <h1 className={style.fraseContent}>Frase qualquer!</h1>
+    <h2 className={style.pStyle}>Outro texto!</h2>
+    </>
+  )
+}
+
+export default Frase;
